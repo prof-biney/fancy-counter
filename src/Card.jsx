3359fe50560330690717bf4ce57d5bdc79cc9fac
abiney1321 <abiney1321@gmail.com>
2025-02-10 23:14:29 +0000

    working on count component

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import Count from "./Count";
+import CountButtons from "./CountButtons";
+import ResetButton from "./ResetButton";
+import Title from "./Title";
+
+export default function Card() {
+  return (
+    <div>
+      <Title />
+      <Count number={5} />
+      <ResetButton />
+
+      <CountButtons />
+    </div>
+  );
+}
--- src/Count.jsx
+++ src/Count.jsx
@@ -1,3 +1,12 @@
-export default function Count() {
-  return <p>0</p>;
+export default function Count({ number }) {
+  return (
+    <div>
+      <CountTitle />
+      <p>{number}</p>
+    </div>
+  );
+}
+
+function CountTitle() {
+  return <h2>Current count is:</h2>;
 }
