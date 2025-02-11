0655872d57ca639e4fdb80d20716aff210c479a6
abiney1321 <abiney1321@gmail.com>
2025-02-11 17:15:05 +0000

    extracting event handler function

--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -1,13 +1,12 @@
 import { ResetIcon } from "@radix-ui/react-icons";
 
 export default function ResetButton({ setCount }) {
+  const handleClick = () => {
+    setCount(0);
+  };
+
   return (
-    <button
-      onClick={() => {
-        setCount(0);
-      }}
-      className="reset-btn"
-    >
+    <button onClick={handleClick} className="reset-btn">
       <ResetIcon className="reset-btn-icon" />
     </button>
   );
