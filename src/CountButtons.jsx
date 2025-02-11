8af338fa24cd9d2b2f90dbe8915539b8b863f95b
abiney1321 <abiney1321@gmail.com>
2025-02-11 16:37:11 +0000

    count button icons added

--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -1,8 +1,14 @@
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+
 export default function CountButtons() {
   return (
     <div className="button-container">
-      <button className="count-btn">-</button>
-      <button className="count-btn">+</button>
+      <button className="count-btn">
+        <MinusIcon className="count-btn-icon" />
+      </button>
+      <button className="count-btn">
+        <PlusIcon className="count-btn-icon" />
+      </button>
     </div>
   );
 }
